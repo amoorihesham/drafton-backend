61ff8a483624cad5f981a1d8e525e5ed6fdb60a1
Amr_Hesham <amrheshammacc@gmail.com>
2026-03-27 04:35:10 +0200

    apply first database-migration

---
+++ src/infrastructure/db/migrations/0000_living_prism.sql
@@ -0,0 +1,29 @@
+CREATE TYPE "public"."user_role" AS ENUM('provider', 'client');--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" text PRIMARY KEY NOT NULL,
+	"email" text NOT NULL,
+	"username" text NOT NULL,
+	"password_hash" text NOT NULL,
+	"role" "user_role" NOT NULL,
+	"is_active" boolean DEFAULT true NOT NULL,
+	"is_email_verified" boolean DEFAULT false NOT NULL,
+	"email_verification_otp" text,
+	"email_verification_otp_expiry" timestamp,
+	"password_reset_otp" text,
+	"password_reset_otp_expiry" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email"),
+	CONSTRAINT "users_username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
+CREATE TABLE "refresh_tokens" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"token" text NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "refresh_tokens_token_unique" UNIQUE("token")
+);
+--> statement-breakpoint
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
